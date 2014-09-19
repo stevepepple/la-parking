8308ab4fb7dc8cbde7c7cd7c0e363b7b0aeb5aba
Steve <spepple@mobileiron.com>
2014-09-18 20:28:09 -0700

    Demo

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express')
 var app = express();
+var cool = require('cool-ascii-faces');
+
+app.use(express.logger());
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send(cool());
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
