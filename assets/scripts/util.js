ba4080f33ed54952f412c6065159e53ac51f140e
Steve <spepple@mobileiron.com>
2014-09-30 22:31:02 -0700

    Function to code address.

--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -7,3 +7,50 @@ String.prototype.toTitleCase = function () {
   	}
   	return string;
 }
+
+/* Meters to Miles */
+if (typeof(Number.prototype.toMiles) === "undefined") {
+  Number.prototype.toMiles = function() {
+    return this * 0.00062137;
+  }
+}
+
+/* Miles to Meters */
+if (typeof(Number.prototype.toMeters) === "undefined") {
+  Number.prototype.toMeters = function() {
+    return this * 1609.34;
+  }
+}
+
+
+function codeAddress(address) {
+	console.log("code this: ", address)
+	geocoder.geocode( { 'address': address}, function(results, status) {
+    	console.log(results, status)
+		
+    	if (status == google.maps.GeocoderStatus.OK) {
+
+			
+			var lat = results[0].geometry.location.lat();
+			var lng = results[0].geometry.location.lng();
+			console.log(results[0].formatted_address, lat, lng)
+			
+			var featureLayer = L.mapbox.featureLayer().addTo(map);
+
+			L.marker([lat, lng]).addTo(featureLayer);
+			
+			map.fitBounds(featureLayer.getBounds());
+			/*
+          	var marker = new google.maps.Marker({
+            	map: parkingMap,
+            	position: results[0].geometry.location
+          	});
+			
+        	parkingMap.setCenter(results[0].geometry.location);
+			*/
+			
+        } else {
+          console.log("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
