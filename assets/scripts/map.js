82e0f70c4c5fbe8bf0c948c6d66ecdfeccf2ae0e
Steve <spepple@mobileiron.com>
2014-10-06 20:48:52 -0700

    Consolidate map-specific view and functions.

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -16,6 +16,31 @@ var mapMain = 'osaez.jhec8j0m';
 
 function init() {
 	
+	App.DirectionsUI = Backbone.View.extend({
+		
+		el: '#directions',
+	
+		template: _.template($("#directions-template").html()),
+	
+		// It's the first function called when this view it's instantiated.
+		initialize: function(options){
+			this.render();
+			this.model = printed_directions;
+		},
+	
+		change: function(event) {
+			console.log("Parks fetched or updated?");
+			this.render();
+		},
+	
+		render: function(){
+
+			 this.$el.html(this.template({ directions : printed_directions.toJSON()} ));
+			 return this;
+		}
+	});
+	
+	
 	/* Have to add a Google Maps instance for the geocoder to work */
     var latlng = new google.maps.LatLng(34.05009932467703, -118.24962615966797);
 	google_map = new google.maps.Map(document.getElementById("map-canvas"), { zoom: 8, center: latlng });
@@ -36,18 +61,35 @@ function init() {
 	
 	/* Get Union Station */
 	
-	/* Bounds of the continetal US 
-		TODO: Use maxBounds: [[77.54209596075547, -3.8671874999999996], [-3.162455530237848, -190.1953125]] ?
-	*/
 	map = L.mapbox.map('map', mapMain, {
 		attributionControl: false,
 		maxZoom: 18,
-		minZoom: 6
+		minZoom: 6,
+		zoomControl: false,
+		scrollWheelZoom: false
 	});
-
-	map.fitBounds([[38.013476231041935, -91.5380859375], [28.806173508854776, -115.24658203125]])
 	
-	getPlace(latlng);
+	overview_Map = L.mapbox.map('overview_map', mapMain, {
+		attributionControl: false,
+		maxZoom: 18,
+		minZoom: 6,
+		zoomControl: false,
+		scrollWheelZoom: false
+	});
+	
+
+	map.fitBounds([[38.013476231041935, -91.5380859375], [28.806173508854776, -115.24658203125]]);
+	
+	parks_list = new App.ParksList();
+	
+	parks_list.fetch({
+        success: function () {
+			/* TODO: Make the list */
+			initParking();
+		}
+	});
+	
+		
 }
 
 function setMapSize() {
